91c3834514a11cbce6a8bdeedeee93537701f6ee
Natcha Phonkamhaeng <natchaphonkamhaeng@192.168.1.4>
2026-03-14 20:16:03 +0700

    add class

--- main.py
+++ main.py
@@ -3,29 +3,44 @@ import random
 # 20 Sep 2025
 # guessing the number between 1-100 with 3 hints
 
-answer = random.randint(1,100) # generate random number between 1 to 100 (inclusive)
-print(answer)
-guessing = 0 # guessing the correct number limit 3 times
+class Game:
+	def __init__(self):
+		self.answer = random.randint(1,100)
+		self.guessing = 0
 
-while True:
+	def game_logic(self):
 
-	num_guess = input("Enter your guessing number: ") # input statement return string
-	num_guess = int(num_guess)
+		while True:
 
-	if num_guess > answer:
-		print(f"Your guessing is over. Guess again!")
-		
-	if num_guess < answer:
-		print(f"Your guessing is less than the answer. Guess again!")
-	guessing += 1
+			try:
+				print(self.answer)
+				num_guess = input("Enter your guessing number: ") # input statement return string
+				num_guess = int(num_guess)
+			
+				if num_guess > self.answer:
+					print(f"Your guessing is over. Guess again!")
+					
+				if num_guess < self.answer:
+					print(f"Your guessing is less than the answer. Guess again!")
+				self.guessing += 1
 
-	print(f"guessing count {guessing}!!!")
-	print()
+				print(f"guessing count {self.guessing}!!!")
+				print()
 
-	if num_guess == answer:
-		print("Congradtulations You won!!")
-		break
+				if num_guess == self.answer:
+					print("Congradtulations You won!!")
+					break
 
-	if guessing == 3:
-		print("You lose!!!")
-		break
+				if self.guessing == 3:
+					print("You lose!!!")
+					break
+
+			except TypeError:
+				print("input number only")
+			except ValueError:
+				print("input number only")
+
+game = Game()
+
+if __name__ == "__main__":
+	game.game_logic()
